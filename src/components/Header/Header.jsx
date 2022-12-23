88017125e67a6fd3912349bca3e1ce20bbc4215b
Matheus Guerreiro <prosadeteclas@gmail.com>
2022-12-22 22:25:40 -0300

    ajustes header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,28 @@ import './header.sass'
 
 // images
 import four from '../../images/sfc_four_logo.png'
-import logo from '../../images/snes_logo_g.png'
+// import logo from '../../images/snes_logo_g.png'
 
 
 const Header = () => {
+
+  let snes = "ntertainment Syste".split("")
+
   return (
     <header className='header'>
       <div className="fourLeft">
         <img src={four} alt="four" />
       </div>
-      <div className="logo">
-        <img src={logo} alt="logo" />
+      <div className="title">
+        {/* <img src={logo} alt="logo" /> */}
+        <h1>Super Nintendo</h1>
+        <div className="es">
+          <div className="i">E</div>
+          {snes?.map((c, i) => {
+            return <div key={i} className="m">{c}</div>
+          })}
+          <div className="f">M</div>
+        </div>
       </div>
       <div className="fourRight">
         <img src={four} alt="four" />
